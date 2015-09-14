afca6a4bfb3640f73f425123a4cb5959a2ecd3df
tom <thomas.william.powers@gmail.com>
2015-09-14 02:56:15 -0400

    fix merge screw up

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 337b10ab299514b0d00a149ff4b7b57ed2dc1b27
 
 <!DOCTYPE html>
 <html lang="en">
@@ -554,50 +550,3 @@
 </body>
 
 </html>
-=======
-<?php 
-	
-	//If both fields have values
-	if(!empty($_GET["year"]) && !empty($_GET["genre"])) {
-		
-		//Set year and genre
-		$year = $_GET["year"];
-		$genre = $_GET["genre"];
-
-		//Include Movie class
-		include 'Models/Movie.php';
-
-		//Instantiate new Movie
-		$movie = new Movie($genre, $year);
-
-		//Generate random movie
-		$randomMovie = $movie->getRandomMovie();
-
-		//Set values
-		$movie->setOriginalTitle($randomMovie);
-		$movie->setOverview($randomMovie);
-		$movie->setImagePath($randomMovie);
-
-		//Get values
-		$title = $movie->getOriginalTitle();
-		$overview = $movie->getOverview();
-		$image_path = $movie->getImagePath();
-
-	//If a year was not chosen
-	} else if (!empty($_GET["genre"]) && empty($_GET["year"])) {
-		
-		$message = "Please select a year";
-		$heading = "Random Movie Generator";
-
-	//No values set
-	} else {
-		
-		$message = "Select a year and then click a genre.";
-		$heading = "Random Movie Generator";
-	}
-
-	//Include view
-	include 'Views/show-movie.php';
-
-?>
->>>>>>> 49b8b6a41a41eb83a921c153798551032f4bd78b
